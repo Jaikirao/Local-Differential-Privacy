3f07b5edc4897a6802a6df1e5c980ed368bf1263
Jaikirao <48488555+Jaikirao@users.noreply.github.com>
2019-07-02 22:54:39 +0530

    Update local_DP.py

--- local_DP.py
+++ local_DP.py
@@ -1,3 +1,2 @@
 #here we start a new project now we complete this project very soon
-
-but i think i am doing this not a good think every i am not thing tha ttype of mistake haha ha
+but i am doijng 
